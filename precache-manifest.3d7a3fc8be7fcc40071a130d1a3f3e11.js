e7eb38a106bc9e4f9b313a1393c3c7092c3f0511
melissa <nadia.mm.mariduena@gmail.com>
2020-08-28 12:44:36 +0200

    Updates

---
+++ precache-manifest.3d7a3fc8be7fcc40071a130d1a3f3e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b65c8f2637990a75362a12a98113617",
+    "url": "/Firegram/index.html"
+  },
+  {
+    "revision": "48bb75ef384cb0a2ae36",
+    "url": "/Firegram/static/css/main.4de89ce6.chunk.css"
+  },
+  {
+    "revision": "847a5003ee4f359a60a0",
+    "url": "/Firegram/static/js/2.180c939a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Firegram/static/js/2.180c939a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48bb75ef384cb0a2ae36",
+    "url": "/Firegram/static/js/main.8f1659f7.chunk.js"
+  },
+  {
+    "revision": "51ac040677bfac14acc5",
+    "url": "/Firegram/static/js/runtime-main.c7c1e1fb.js"
+  }
+]);
