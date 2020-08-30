3f9789008e4c732126cf421ca5a551cacdc63904
melissa <nadia.mm.mariduena@gmail.com>
2020-08-30 12:53:19 +0200

    progress bar| set it to null after download completed

--- src/comps/progressBar.js
+++ src/comps/progressBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useStorage from "../hooks/useStorage";
+import { useEffect } from "react";
 
 //
 //
@@ -53,8 +54,82 @@ const ProgressBar = ({ file, setFile }) => {
     
                                     and in that moment in time we get values back for progress and then when 
                                     its DONE the URL as well.
+                                    --------------------------------------------------------
+
+
+
+
+
+
+
+
     */
-  return <div className="progress-bar">progress</div>;
+
+  useEffect(
+    () => {
+      // if we have a valid value (thats is not null), at that point we want  to set the file and
+      // set the file to be EQUAL to NULL
+      if (url) {
+        //   this if condition, will make the PROGRESS BAR disappear after the download of the file is completed
+        setFile(null);
+      }
+    },
+    [url],
+    setFile
+    // You need to add the SETFILE here because you are using it inside the statement
+  );
+  /*
+
+
+                                                seEffect(() => {  }, [url]);
+  
+                                     Here you will pass a FUNCTION that will run when the URL changes
+  
+  
+  
+  
+  */
+
+  return (
+    <div className="progress-bar" style={{ width: progress + "%" }}></div>
+    /*
+
+
+
+
+
+
+                                                    style={{ width: progress + "%" }}>    
+       
+       
+                                    CONCERNS the progression bar, when you will download an image, while
+                                    the image is progressing the width will grow in the porcentage. the 
+                                    height of the with is in the css
+                                    --------------------------------------------------
+
+                               **    REMOVING THE PROGRESS bar once it reaches the 100%
+                               as we dont want that it remains there.
+
+                               to remove it, we need to SET the value of file back to null
+                               
+                               line 7:
+                               ({ file, 
+
+                               because remember we only show the PROGRESS BAR if the "file" has a value
+                               (if the user downloaded something). SO we can set the value back to null
+                               by using this function:
+
+                               line: 7:
+                               setFile })
+
+                               we will do that when we will have a URL to use, because at that point we 
+                               know the file is fully uploaded, because we only get that FILE after 
+                               the upload is COMPLETED.
+
+
+
+*/
+  );
 };
 
 export default ProgressBar;
